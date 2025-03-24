38feda8b94f56f52a89db5882e6fc9287726fea1
gundlachj <gundlachj@msoe.edu>
2025-03-23 21:06:46 -0400

    Implemented square braces

--- src/main/java/com/jggundl/parenthesis/ValidParenthesis.java
+++ src/main/java/com/jggundl/parenthesis/ValidParenthesis.java
@@ -30,6 +30,10 @@ public class ValidParenthesis {
                 this.stack.push();
             } else if (str.charAt(i) == '}') {
                 this.stack.pop();
+            } else if (str.charAt(i) == '[') {
+                this.stack.push();
+            } else if (str.charAt(i) == ']') {
+                this.stack.pop();
             }
         }
 
